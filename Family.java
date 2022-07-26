2cf7bbd922d79abc52f89bc3d7360eb7c53657e5
Santhaprakash <82021459+santhaprakash@users.noreply.github.com>
2022-07-26 07:48:09 +0530

    Create Family.java

---
+++ Family.java
@@ -0,0 +1,8 @@
+public class Family{
+    public String name="";
+    public String gender="";
+    public Family fatherName;
+    public Family motherName;
+    public Family child[]=new Family[50];
+    public int childcount=0;
+}
